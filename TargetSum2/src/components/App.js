72376d1d91d019a0fa0622c8c35ff56c57bc65d4
Mikayla Lafond <mikayla.lafond@gmail.com>
2023-03-01 10:29:49 -0600

    Finished Videos 3 & 4
    - Got ES Lint Running and it's helping lots
    - Got the game code started and have a random number generating to the screen on refresh

--- TargetSum2/src/components/App.js
+++ TargetSum2/src/components/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-
-import {View, Text} from 'react-native';
+import Game from './Game.js';
 
 class App extends React.Component {
   render() {
-    return (
-      <View>
-        <Text>123456</Text>
-      </View>
-    );
+    return <Game />;
   }
 }
 
---
+++ TargetSum2/src/components/Game.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {View, Text, StyleSheet} from 'react-native';
+
+class Game extends React.Component {
+  target = 10 + Math.floor(40 * Math.random());
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.target}>{this.target}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ddd',
+    flex: 1,
+    paddingTop: 100,
+  },
+
+  target: {
+    fontSize: 40,
+    backgroundColor: '#aaa',
+    marginHorizontal: 50,
+    textAlign: 'center',
+  },
+});
+
+export default Game;
