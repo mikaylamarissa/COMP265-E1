31d6db35dd7550c8b98ff6e67d6b45efc1c58f02
Mikayla Lafond <mikayla.lafond@gmail.com>
2023-03-01 18:21:29 -0600

    finsihed project. Some things not working so need to work on debugging later

--- TargetSum2/src/components/App.js
+++ TargetSum2/src/components/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 import Game from './Game.js';
 
 class App extends React.Component {
+  state = {
+    gameId: 1, };
+  resetGame = () => {
+    this.setState((prevState) => {
+      return { gameId: prevState.gameId +1};
+    });
+  };
   render() {
-    return <Game randomNumberCount={6} initialSeconds={10}/>;
+    return <Game key={this.state.gameId} onPlayAgain={this.resetGame} randomNumberCount={6} initialSeconds={10}/>;
   }
 }
 
--- TargetSum2/src/components/Game.js
+++ TargetSum2/src/components/Game.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Button, Text, StyleSheet } from 'react-native';
 
 import RandomNumber from './RandomNumber';
+import shuffle from 'lodash.shuffle';
 
 class Game extends React.Component {
   static propTypes = {
     randomNumberCount: PropTypes.number.isRequired,
     initialSeconds: PropTypes.number.isRequired,
+    onPlayAgain: PropTypes.func.isRequired,
   };
   state = {
     selectedIDs: [],
-    remainingSeconds: this.props.initialSeconds,
+    remainingSeconds: this.props.initialSecondFfs,
   };
+  gameStatus = 'PLAYING';
   randomNumbers = Array.from({ length: this.props.randomNumberCount }).map(() => 1 + Math.floor(10 * Math.random()));
   target = this.randomNumbers.slice(0, this.props.randomNumberCount - 2).reduce((acc, curr) => acc + curr, 0);
-
+  shuffledRandomNumbers = shuffle(this.randomNumbers);
   componentDidMount() {
     this.intervalId = setInterval(() => {
       this.setState((prevState) => { return { remainingSeconds: prevState.remainingSeconds - 1 }; }, () => { if (this.state.remainingSeconds === 0) { clearInterval(this.intervalId); } });
     }, 1000);
   }
+  //reset game
+
+
 
   componentWillUnmount() {
     clearInterval(this.intervalId);
@@ -36,12 +42,16 @@ class Game extends React.Component {
     }));
 
   }
+  // eslint-disable-next-line react/no-deprecated
+  componentWillUpdate(nextProps, nextState){
+    if (nextState.selectedIDs !== this.state.selectedIDs || nextState.remainingSeconds === 0) { this.gameStatus = this.calcgameStatus(nextState); if (this.gameStatus !== 'PLAYING'){clearInterval(this.intervalId);}}} 
+  
   // GameStatus
-  gameStatus = () => {
-    const sumSelected = this.state.selectedIDs.reduce((acc, curr) => {
+  calcgameStatus = (nextState) => {
+    const sumSelected = nextState.selectedIDs.reduce((acc, curr) => {
       return acc + this.randomNumbers[curr];
     }, 0);
-    if (this.state.remainingSeconds === 0) {
+    if (nextState.remainingSeconds === 0) {
       return 'LOST';
     }
     if (sumSelected < this.target) {
@@ -55,7 +65,7 @@ class Game extends React.Component {
     }
   };
   render() {
-    const gameStatus = this.gameStatus();
+    const gameStatus = this.calcgameStatus;
     return (
       <View style={styles.container}>
         <Text style={[styles.target, styles[`STATUS_${gameStatus}`]]}>{this.target}</Text>
@@ -64,6 +74,8 @@ class Game extends React.Component {
             <RandomNumber key={index} id={index} number={randomNumbers} isDisabled={this.isNumberSelected(index) || gameStatus !== 'PLAYING'} onPress={this.selectNumber} />
           )}
         </View>
+        {this.gameStatus !== 'PLAYING' && (<Button title = "Play Again" onPress = {this.props.onPlayAgain} />
+        )}
         <Text style={styles.timer}>{this.state.remainingSeconds}</Text>
       </View>
     );
